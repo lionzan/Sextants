ffdeadc4f806202a2a9ddd0d011c20ab9c5c09d6
Leonardo Zangrando <leo@zangrando.com>
2019-03-02 08:35:02 +0000

    Create pyConvert.py

---
+++ digital/data/pyConvert.py
@@ -0,0 +1,24 @@
+output = []
+items = []
+fileIn = open('StarsData.csv', 'r')
+for row in fileIn.read().split('\n'):
+    items.append[row.split(',')]
+for item in items:
+    output.append(float(item[0])
+    output.append(float(item[1])
+    output.append(float(item[2])
+    output.append(item[3])
+    output.append(item[4])
+    
+    
+from array import array
+output_file = open('StarsData.bin', 'wb')
+float_array = array('d', [3.14, 2.7, 0.0, -1.0, 1.1])
+float_array.tofile(output_file)
+output_file.close()
+
+s = bytes(s, 'utf-8')    # Or other appropriate encoding
+struct.pack("I%ds" % (len(s),), len(s), s)
+
+    
+    https://stackoverflow.com/questions/18367007/python-how-to-write-to-a-binary-file
