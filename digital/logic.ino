4ef4e67c752d3e7954db59baaa40a5c3880faf17
Leonardo Zangrando <leo@zangrando.com>
2019-03-09 17:07:53 +0000

    Functioning mode selector
    Read button events and Changes across modes

--- digital/logic.ino
+++ digital/logic.ino
@@ -2,17 +2,21 @@
 #define NUMBUTTONS 4 //S, L, H, L+H
 #define DEBOUNCE 5  // how many ms to debounce, 5+ ms is usually plenty
  
-//define the buttons that we'll use.
+//define the buttons
 byte buttons[] = {4, 5, 6}; // physical buttons CONFIRM, UP, DOWN
 char bNames[] ="CUDJ"; // buttons names CONFIRM, UP,  DOWN, UP+DOWN
 char bEvents[] ="pslr"; // event names PRESS, RELEASE AFTER SHORT, BEGIN LONG, RELEASE AFTER LONG
 int longStart = 500; // delay before considered long press
+boolean noButtons = true; // no buttons pressed
  
-//track if a button is just pressed, just released, or 'currently pressed' for S, L, H, L+H
+//track if a button is just pressed, just released, or 'currently pressed'
 byte pressed[NUMBUTTONS], oldPressed[NUMBUTTONS];
 byte justPressed[NUMBUTTONS], justReleased[NUMBUTTONS];
 byte longPress[NUMBUTTONS], oldLongPress[NUMBUTTONS];
-byte previousKeyState[NUMBUTTONS], currentKeyState[NUMBUTTONS];
+
+// current mode
+char modeNames[] = "FMPS"; // FIND, MEASURE, POSITION, SETUP . SF, TS, PF, SU
+byte mode = 1; //start from mode STARFINDER
 
 void setup() {
   byte i;
@@ -28,14 +32,74 @@ void setup() {
 }
 
 void loop() {
-  char event[2] = "";
-  if (checkEvent(event)) {
-    Serial.print(event[0]);
-    Serial.println(event[1]);
+  byte event1 = checkEvent1();
+  if (event1 != 255) {
+    Serial.print(event1, HEX);
+    Serial.print(" ");
+    Serial.print(modeNames[mode-1]);
+    Serial.print(" ");
+    switch (mode) {
+      case 1: {  // STARFINDER
+        switch (event1) {
+          case 0x12 : { // 00 Confirm short release 
+            mode = 2;
+            break;
+          }
+          case 0x14 : { // 02 Confirm long release 
+            mode = 3;
+            break;
+          }
+          case 0x44 : { // 00 Joint long release 
+            mode = 4;
+            break;
+          }
+        }
+        break;
+      }
+      case 2: {  // TAKE SIGHT OR MEASURE
+        switch (event1) {
+          case 0x12 : { // 00 Confirm short release 
+            mode = 1;
+            break;
+          }
+          case 0x44 : { // 00 Joint long release 
+            mode = 1;
+            break;
+          }
+        }
+        break;
+      }
+      case 3: {  // POSITION FIXING
+        switch (event1) {
+          case 0x12 : { // Confirm short release 
+            mode = 1;
+            break;
+          }
+          case 0x44 : { // 00 Joint long release 
+            mode = 4;
+            break;
+          }
+        }
+        break;
+      }
+      case 4: {  // SETUP
+        switch (event1) {
+          case 0x44 : { // 00 Joint long release 
+            mode = 1;
+            break;
+          }
+        }
+        break;
+      }
+
+    }
+//    Serial.print(event[0]);
+//    Serial.print(event[1]);
+    Serial.println(modeNames[mode-1]);
   }
 }
  
-void check_switches() {
+void checkSwitches() {
   static byte previousState[NUMBUTTONS];
   static byte currentState[NUMBUTTONS];
   static long lastTime;
@@ -78,6 +142,7 @@ void check_switches() {
     }
     previousState[index] = currentState[index]; //keep a running tally of the buttons
   }
+}
 
  boolean checkEvent(char event[2]) {
   boolean isEvent = false;
@@ -116,5 +181,40 @@ void check_switches() {
     oldPressed[i]=pressed[i];
     oldLongPress[i]=longPress[i];
   }
+  noButtons = ((pressed[0]==0) && (pressed[1]==0) && (pressed[2]==0) && (pressed[3]==0));
   return isEvent;
+}
+
+
+ byte checkEvent1() {
+  byte event = 255; // if no event return 255
+  checkSwitches();
+  for (int i=4; i>0; i--) { 
+    if (longPress[i-1]!=oldLongPress[i-1]) {
+      if (longPress[i-1]==1) { // start long
+        event = 3 + 16 * i;
+        break;
+      }
+      if (longPress[i-1]==0) { // released long
+        event = 4 + 16 * i;
+        break;
+      }
+    }
+    if (pressed[i-1]!=oldPressed[i-1]) {
+      if (pressed[i-1]==1) {// pressed
+        event = 1 + 16 * i;
+        break;
+      }
+      if (pressed[i-1]==0) {// released
+        event = 2 + 16 * i;
+        break;
+      }
+    }
+  }
+  for (int i=0; i<4; i++) {
+    oldPressed[i]=pressed[i];
+    oldLongPress[i]=longPress[i];
+  }
+  noButtons = ((pressed[0]==0) && (pressed[1]==0) && (pressed[2]==0) && (pressed[3]==0));
+  return event;
 }
